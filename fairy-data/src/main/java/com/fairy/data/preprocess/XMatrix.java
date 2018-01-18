4204830c456c0600755f71961b70f4e00f863768
Fairy_LFen <759128659@qq.com>
2018-01-18 14:02:21 +0800

    数据融合

--- fairy-data/src/main/java/com/fairy/data/preprocess/XMatrix.java
+++ fairy-data/src/main/java/com/fairy/data/preprocess/XMatrix.java
@@ -19,18 +19,79 @@ import java.util.List;
 public class XMatrix {
 
     private String poiPath = ConfigUtil.getValue("poiPath", "conf.properties");
+    private String poiSimplePath = ConfigUtil.getValue("poiSimplePath", "conf.properties");
     private String regionsPath = ConfigUtil.getValue("regionsPath", "conf.properties");
     private FileUtil fileUtil = FileUtil.getInstance();
+    private int poi_v;
+
+    public void initParam(){
+        String poiVStr = ConfigUtil.getValue("poi_v", "conf.properties");
+        poi_v = Integer.valueOf(poiVStr);
+    }
+
 
     public void XMatrixGen() throws IOException {
+        initParam();
+
         String regionStr = fileUtil.readJsonFileToStr(new File(regionsPath));
         List<Region> regionList = JSON.parseArray(regionStr, Region.class);
 
-        String poiStr = fileUtil.readJsonFileToStr(new File(poiPath));
-        List<POI> poiList = JSON.parseArray(poiStr, POI.class);
+        String poiSimpleStr = fileUtil.readJsonFileToStr(new File(poiSimplePath));
+        List<POISimple> poiSimpleList = JSON.parseArray(poiSimpleStr, POISimple.class);
 
+
+        int[][] matrixX = new int[regionList.size()][poi_v];
+        Region region = null;
+        for (int i = 0; i < regionList.size(); i++) {
+            region = regionList.get(i);
+            for (POISimple poi : poiSimpleList) {
+                if(isContained(region, poi)){
+                    int poiIndex = toPOIIndex(poi);
+                    if(poiIndex > 0){
+                        matrixX[i][poiIndex - 5]++;
+                    }
+
+                }
+            }
+        }
+
+        String jsonStr = JSON.toJSONString(matrixX);
+        fileUtil.saveToFile("fairy-data/data/XMatrix.json", jsonStr, false);
     }
 
+    /**
+     * 获取poi类型，转为数组下标
+     * @param poi
+     * @return
+     */
+    private int toPOIIndex(POISimple poi) {
+        try {
+            String typeCode = poi.getTypecode();
+            String subCode = typeCode.substring(0, 2);
+            int index = Integer.valueOf(subCode);
+            if (index <= 20 && index >= 1) {
+                return index;
+            } else {
+                return -1;
+            }
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private boolean isContained(Region region, POISimple poi) {
+        double lat = poi.getLocationx();
+        double lng = poi.getLocationy();
+
+        if(lat >= region.getLat02() && lat <= region.getLat01() && lng >= region.getLng01()
+                && lng <= region.getLng02())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    //将POI转为POISimple
     public void simplePOIData() throws IOException {
         String poiStr = fileUtil.readJsonFileToStr(new File(poiPath));
         List<POI> poiList = JSON.parseArray(poiStr, POI.class);
@@ -46,7 +107,7 @@ public class XMatrix {
     }
 
     public static void main(String[] args) throws IOException {
-//        new XMatrix().XMatrixGen();
-        new XMatrix().simplePOIData();
+        new XMatrix().XMatrixGen();
+//        new XMatrix().simplePOIData();
     }
 }
--- fairy-data/src/main/java/com/fairy/pojo/Region.java
+++ fairy-data/src/main/java/com/fairy/pojo/Region.java
@@ -4,40 +4,40 @@ package com.fairy.pojo;
  * 由四叉树分割后的地理区域
  */
 public class Region {
-    public String lat01;
-    public String lng01;
-    public String lat02;
-    public String lng02;
+    public double lat01;
+    public double lng01;
+    public double lat02;
+    public double lng02;
 
-    public String getLat01() {
+    public double getLat01() {
         return lat01;
     }
 
-    public void setLat01(String lat01) {
+    public void setLat01(double lat01) {
         this.lat01 = lat01;
     }
 
-    public String getLng01() {
+    public double getLng01() {
         return lng01;
     }
 
-    public void setLng01(String lng01) {
+    public void setLng01(double lng01) {
         this.lng01 = lng01;
     }
 
-    public String getLat02() {
+    public double getLat02() {
         return lat02;
     }
 
-    public void setLat02(String lat02) {
+    public void setLat02(double lat02) {
         this.lat02 = lat02;
     }
 
-    public String getLng02() {
+    public double getLng02() {
         return lng02;
     }
 
-    public void setLng02(String lng02) {
+    public void setLng02(double lng02) {
         this.lng02 = lng02;
     }
 }
